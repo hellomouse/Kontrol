b8bf2e8520e00495ef995d819c91ee6f703d65d0
Bowserinator <bowserinator@gmail.com>
2021-02-09 19:06:49 -0500

    RLC Circuit tests

--- src/main/java/net/hellomouse/kontrol/electrical/circuit/virtual/tests/RLCCircuitTests.java
+++ src/main/java/net/hellomouse/kontrol/electrical/circuit/virtual/tests/RLCCircuitTests.java
@@ -5,9 +5,11 @@ import net.hellomouse.kontrol.electrical.circuit.virtual.components.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import static net.hellomouse.kontrol.electrical.circuit.virtual.VirtualCircuitConstants.DT;
-import static net.hellomouse.kontrol.electrical.circuit.virtual.tests.TestConstants.EPSILON;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
@@ -16,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class RLCCircuitTests {
     @Test
-    @DisplayName("Overdamped circuit: R = 10, L = 0.1, C = 0.1")
+    @DisplayName("Underdamped circuit: R = 10, L = 1.5, C = 0.01")
     void test1() {
         VirtualCircuit circuit = new VirtualCircuit();
 
@@ -34,19 +36,147 @@ class RLCCircuitTests {
         circuit.addComponent(new VirtualGround(), 0, 0);
         circuit.solve();
 
-        // TODO
-        // Steady state is correct
-        // [0.0, 9.999999999999998, 9.999999999989999, 1.0000997616784776E-7, 9.999997616794777E-8, 9.999997615794778E-8]
+        ArrayList<Double> R1Voltage = new ArrayList<>();
 
-        for (int i = 0; i < 2 / DT; i++) {
+        for (int i = 0; i < 3 / DT; i++) {
             circuit.tick();
             circuit.solve();
-            System.out.println(i + "," + R1.getVoltage());
+            R1Voltage.add(R1.getVoltage());
         }
 
+        System.out.println("Expected: A sinusoid that goes up, then down and decays\n");
+        graph(R1Voltage, 1);
+    }
 
-        double I = -(10.0 - 2) / 10.0;
-        // assertEquals(I, R1.getCurrent(), EPSILON);
-        assertEquals(-10 + 1.4, R1.getVoltage(), EPSILON);
+    @Test
+    @DisplayName("More underdamped circuit: R = 0.1, L = 1.5, C = 0.01")
+    void test2() {
+        VirtualCircuit circuit = new VirtualCircuit();
+
+        VirtualCapacitor C1 = new VirtualCapacitor(0.01);
+        VirtualInductor L1 = new VirtualInductor(1.5);
+        VirtualResistor R1 = new VirtualResistor(0.1);
+        VirtualVoltageSource V1 = new VirtualVoltageSource(10);
+
+        circuit.addComponent(V1, 1, 0);
+        circuit.addComponent(new VirtualResistor(0.001), 1, 2);
+        circuit.addComponent(C1, 2, 3);
+        circuit.addComponent(new VirtualResistor(0.001), 3, 4);
+        circuit.addComponent(L1, 4, 5);
+        circuit.addComponent(R1, 5, 0);
+        circuit.addComponent(new VirtualGround(), 0, 0);
+        circuit.solve();
+
+        ArrayList<Double> R1Voltage = new ArrayList<>();
+
+        for (int i = 0; i < 5 / DT; i++) {
+            circuit.tick();
+            circuit.solve();
+            R1Voltage.add(R1.getVoltage());
+        }
+
+        System.out.println("Expected: A sinusoid that goes up, then down and decays slowly\n");
+        graph(R1Voltage, 1);
+    }
+
+    @Test
+    @DisplayName("Underdamped circuit with 0.7 V diode: R = 1, L = 1.5, C = 0.01")
+    void test3() {
+        VirtualCircuit circuit = new VirtualCircuit();
+
+        VirtualCapacitor C1 = new VirtualCapacitor(0.01);
+        VirtualInductor L1 = new VirtualInductor(1.5);
+        VirtualResistor R1 = new VirtualResistor(1);
+        VirtualVoltageSource V1 = new VirtualVoltageSource(10);
+        VirtualDiode D1 = new VirtualDiode(0.7);
+
+        circuit.addComponent(V1, 1, 0);
+        circuit.addComponent(new VirtualResistor(0.001), 1, 2);
+        circuit.addComponent(C1, 2, 3);
+        circuit.addComponent(new VirtualResistor(0.001), 3, 4);
+        circuit.addComponent(L1, 4, 5);
+        circuit.addComponent(R1, 5, 6);
+        circuit.addComponent(D1, 6, 0);
+        circuit.addComponent(new VirtualGround(), 0, 0);
+        circuit.solve();
+
+        ArrayList<Double> R1Voltage = new ArrayList<>();
+
+        for (int i = 0; i < 5 / DT; i++) {
+            circuit.tick();
+            circuit.solve();
+            R1Voltage.add(R1.getVoltage());
+        }
+
+        System.out.println("A graph that curves down at the beginning then asymptotically approaches zero\n");
+        graph(R1Voltage, 1);
+    }
+
+    @Test
+    @DisplayName("Overdamped circuit: R = 10, L = 1.5, C = 1")
+    void test4() {
+        VirtualCircuit circuit = new VirtualCircuit();
+
+        VirtualCapacitor C1 = new VirtualCapacitor(1);
+        VirtualInductor L1 = new VirtualInductor(1.5);
+        VirtualResistor R1 = new VirtualResistor(10);
+        VirtualVoltageSource V1 = new VirtualVoltageSource(10);
+
+        circuit.addComponent(V1, 1, 0);
+        circuit.addComponent(new VirtualResistor(0.001), 1, 2);
+        circuit.addComponent(C1, 2, 3);
+        circuit.addComponent(new VirtualResistor(0.001), 3, 4);
+        circuit.addComponent(L1, 4, 5);
+        circuit.addComponent(R1, 5, 0);
+        circuit.addComponent(new VirtualGround(), 0, 0);
+        circuit.solve();
+
+        ArrayList<Double> R1Voltage = new ArrayList<>();
+
+        for (int i = 0; i < 100 / DT; i++) {
+            circuit.tick();
+            circuit.solve();
+            R1Voltage.add(R1.getVoltage());
+        }
+
+        System.out.println("A graph that goes asymptotically to zero without crossing zero\n");
+        graph(R1Voltage, 16);
+    }
+
+    /**
+     * Draw an ASCII graph given a list of value to plot, where index is the x axis and value is scaled
+     * to a fixed graph height and size
+     * @param values Values to plot
+     * @param keep Keep every nth value, use to compress graph horizontally
+     */
+    private void graph(ArrayList<Double> values, int keep) {
+        final int GRAPH_HEIGHT = 21;
+        final int GRAPH_WIDTH = values.size() / keep + 1;
+        String[] graph = new String[GRAPH_HEIGHT];
+
+        // Generate graph
+        for (int y = 0; y < GRAPH_HEIGHT; y++) {
+            graph[y] = y == GRAPH_HEIGHT / 2 ?
+                String.join("", Collections.nCopies(GRAPH_WIDTH, "-")) :
+                String.join("", Collections.nCopies(GRAPH_WIDTH, " "));
+        }
+
+        // Plot values. May be inefficient but we're not plotting large plots
+        double maxValue = Collections.max(values.stream().map(Math::abs).collect(Collectors.toList()));
+
+        for (int i = 0; i < values.size(); i++) {
+            if (i % keep != 0)
+                continue;
+
+            double value = values.get(i);
+            int scaledHeight = (int)Math.round(value / (maxValue * 2) * GRAPH_HEIGHT + (float)GRAPH_HEIGHT / 2);
+
+            char[] chars = graph[scaledHeight].toCharArray();
+            chars[i / keep] = 'X';
+            graph[scaledHeight] = String.valueOf(chars);
+        }
+
+        for (String line : graph)
+            System.out.println(line);
     }
 }
