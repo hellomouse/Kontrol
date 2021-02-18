2d817ed9984fd5aad0eec8912b93711ccf3c9ca0
Bowserinator <bowserinator@gmail.com>
2021-02-18 02:05:17 -0500

    VoxelShape Utils + push button voxel shape
    Signed-off-by: Bowserinator <bowserinator@gmail.com>

--- src/main/java/net/hellomouse/kontrol/electrical/block/BasicPushButtonBlock.java
+++ src/main/java/net/hellomouse/kontrol/electrical/block/BasicPushButtonBlock.java
@@ -1,5 +1,6 @@
 package net.hellomouse.kontrol.electrical.block;
 
+import net.hellomouse.kontrol.util.VoxelShapeUtil;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
@@ -13,24 +14,24 @@ import net.minecraft.world.BlockView;
 
 @SuppressWarnings({"deprecation"})
 public class BasicPushButtonBlock extends AbstractPushButtonBlock {
-
     public static final VoxelShape CUBOID_BASE, UNPRESSED_SHAPE, PRESSED_SHAPE;
 
     static {
         CUBOID_BASE = VoxelShapes.union(
-                createCuboidShape(0, 0, 0, 16, 1, 16),
-                createCuboidShape(2, 1, 2, 14, 4, 14));
+                createCuboidShape(0, 0, 0, 16, 1, 16),  // Base
+                createCuboidShape(2, 1, 2, 14, 4, 14),  // Button housing
+                createCuboidShape(6, 1, 15, 10, 9, 16), // Side connectors
+                createCuboidShape(6, 1, 0, 10, 9, 1));  // Side connectors
         UNPRESSED_SHAPE = createCuboidShape(4, 4, 4, 12, 7, 12);
         PRESSED_SHAPE = createCuboidShape(4, 4, 4, 12, 5, 12);
     }
 
-    public BasicPushButtonBlock(AbstractBlock.Settings settings) {
-        super(settings);
-    }
+    public BasicPushButtonBlock(AbstractBlock.Settings settings) { super(settings); }
 
     @Override
     public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, ShapeContext context) {
         Direction facing = blockState.get(Properties.HORIZONTAL_FACING);
-        return VoxelShapes.union(CUBOID_BASE, blockState.get(AbstractPushButtonBlock.PRESSED) ? PRESSED_SHAPE : UNPRESSED_SHAPE);
+        boolean pressed = blockState.get(AbstractPushButtonBlock.PRESSED);
+        return VoxelShapeUtil.rotateShape(facing, VoxelShapes.union(CUBOID_BASE, pressed ? PRESSED_SHAPE : UNPRESSED_SHAPE));
     }
 }
---
+++ src/main/java/net/hellomouse/kontrol/util/VoxelShapeUtil.java
@@ -0,0 +1,56 @@
+package net.hellomouse.kontrol.util;
+
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+
+
+/**
+ * Utilities for dealing with VoxelShapes
+ * @author Bowserinator
+ */
+public class VoxelShapeUtil {
+    /**
+     * Rotate the "NORTH" end of the shape so it faces in Direction "to" instead.
+     * In the event a shape has multiple ending orientations that are all valid (ie, rotating so
+     * NORTH -> UP can have 4 distinct rotations that all point up), the one that requires the least
+     * rotations will be done.
+     *
+     * @param to Direction original north end of shape now points.
+     * @param shape Shape to rotate
+     * @return Rotated shape
+     */
+    public static VoxelShape rotateShape(Direction to, VoxelShape shape) {
+        if (to == Direction.NORTH)
+            return shape;
+
+        final VoxelShape[] newShape = { VoxelShapes.empty() };
+
+        shape.forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> {
+            switch(to) {
+                case WEST:
+                    // Swap X <-> Z
+                    newShape[0] = VoxelShapes.union(newShape[0], VoxelShapes.cuboid(minZ, minY, minX, maxZ, maxY, maxX));
+                    break;
+                case EAST:
+                    // Swap X <-> Z, then take 1 - newX
+                    newShape[0] = VoxelShapes.union(newShape[0], VoxelShapes.cuboid(1 - minZ, minY, minX, 1 - maxZ, maxY, maxX));
+                    break;
+                case SOUTH:
+                    // Z = 1 - Z
+                    newShape[0] = VoxelShapes.union(newShape[0], VoxelShapes.cuboid(minX, minY, 1 - minZ, maxX, maxY, 1 - maxZ));
+                    break;
+                case UP:
+                    // Swap Y, Z, then take 1 - newY
+                    newShape[0] = VoxelShapes.union(newShape[0], VoxelShapes.cuboid(minX, 1 - minZ, minY, maxX, 1 - maxZ, maxY));
+                    break;
+                case DOWN:
+                    // Swap Y, Z
+                    newShape[0] = VoxelShapes.union(newShape[0], VoxelShapes.cuboid(minX, minZ, minY, maxX, maxZ, maxY));
+                    break;
+            }
+        });
+
+        return newShape[0];
+    }
+}
